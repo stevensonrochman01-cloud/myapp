b3fcbd4fa214f3c48756996574b50453383653d6
stevensonrochman01-cloud <stevenson.rochman01@gmail.com>
2026-01-10 00:13:23 +0000

    Update apply.js

--- api/apply.js
+++ api/apply.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
       }
     }
 
-    if (body.is18 !== true || body.consent !== true) {
+    if (body.is18 !== true ) {
       return res.status(400).send('18+ confirmation and consent are required.');
     }
 
